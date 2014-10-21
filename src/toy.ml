d643cc91391f063052866d8a70d40a44d8924c33
Xavier Montillet <xavierm02.net@gmail.com>
2014-10-21 20:58:18 +0200

    Modified Makefile, toy.ml and added demo to comply with guidelines.

--- src/toy.ml
+++ src/toy.ml
@@ -1,3 +1,15 @@
 let () =
-  let file = try Sys.argv.(1) with _ -> "exemples/test.toy" in
-  Step.go_step file
+  let files =
+    match Sys.argv |> Array.to_list with
+    | [] -> ["exemples/test.toy"]
+    | _ :: l -> l
+  in
+  files |> List.iter (fun file ->
+    let tmp = "+" ^ (String.make (2 + String.length file)) '-' ^ "+\n" in
+    "\n" ^
+    tmp ^
+    "| " ^ file ^ " |\n" ^
+    tmp ^
+    "\n" |> print_string;
+    Step.go_step file;
+  )
